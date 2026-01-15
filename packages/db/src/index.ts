206460ea4a2d23da9076251ef8809835b25c9d5f
Aske Gudmand-Høyer <aske@kultunaut.dk>
2026-01-15 10:07:46 +0100

    fix: update database connection handling and add dummy connection string

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,13 @@
 import { drizzle } from 'drizzle-orm/bun-sql';
 import { SQL } from 'bun';
 import * as schema from './schema';
+import { building } from '$app/environment';
 
 const connectionString = process.env.DATABASE_URL;
-if (!connectionString) throw "Please setup DATABASE_URL environment variable"
+if (!connectionString && !building) throw "Please setup DATABASE_URL environment variable"
 
 const client = new SQL(process.env.DATABASE_URL!);
+
 export const db = drizzle(client, { schema });
 
 export * from './schema';
